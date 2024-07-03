28fb992b8cac2316912ebf9bfb481ecc761045ce
DammyNova7 <nwalagaddammy7@gmail.com>
2024-07-03 22:14:19 +0100

    Fix: app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def get_endpoint():
         
 
         response = {
-            'client ip': visitor_ip,
-            'location': city,
-            'greeting': f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {city}"
+            "client ip": visitor_ip,
+            "location": city,
+            "greeting": f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {city}"
         }
     except requests.RequestException as e:
         location_info = {"error": "Unable to fetch location data"}
