417691c1137e088901a86782bcf8c3594e34525d
DammyNova7 <nwalagaddammy7@gmail.com>
2024-07-03 22:08:10 +0100

    Fix: app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 #importing modules
 from flask import Flask, request, jsonify
-from collections import OrderedDict
 import requests
 
 
@@ -26,13 +25,14 @@ def get_endpoint():
         weather_api_key = '2cd8bb028638cff45e4cb2c285a26a90'
         weather_info = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric").json()
         temperature = round(weather_info['main']['temp']) if 'main' in weather_info else "Not available"
-        greeting = f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {city}"
+        
 
-        response = OrderedDict({
+        response = {
             'client ip': visitor_ip,
-            'location': city,
-            'greeting': greeting
-        })
+            #'location': city,
+            'greeting': f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {city}",
+            'location': city
+        }
     except requests.RequestException as e:
         location_info = {"error": "Unable to fetch location data"}
 
